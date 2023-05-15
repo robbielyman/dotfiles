aa31ac6795c87c31180a5e65729dc4fba44ad12d
Rylee Lyman <rylee.alanza@gmail.com>
2023-05-15 15:21:04 -0400

    load config in org.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,3 @@
-;;; init.el --- Emacs configuration file -*- lexical-binding: t; -*-
-
-;; Author: Rylee Lyman
-
-;;; Commentary:
-
-;;; Code:
-
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -22,18 +14,12 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
-;; (when (memq window-system '(mac ns x))
-;;  (exec-path-from-shell-initialize))
-;; (exec-path-from-shell-copy-env "DBUS_SESSION_BUS_ADDRESS")
-
-(setq catppuccin-flavor 'frappe)
-(catppuccin-reload)
 
 (eval-when-compile
   (require 'use-package)
   (setq use-package-always-ensure t))
 
-(unless (package-installed-p 'use-package)
+(unless (package-installed-p 'usepackage)
   (package-refresh-contents)
   (package-install 'use-package))
 
@@ -41,106 +27,38 @@
   :ensure nil
   :preface
   (defvar ryl/indent-width 2)
+  (defun ryl/check-darwin (filename)
+    (if (equal system-type 'darwin) filename "no"))
+  (defun ryl/check-linux (filename)
+    (if (equal system-type 'gnu/linux) filename "no"))
+  :custom
+  (minibuffer-prompt-properties '(read-only t cursor-intangible t))
+  (completion-cycle-threshold 3)
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  (tab-always-indent 'complete)
+  (enable-recursive-minibuffers t)
+  (scroll-margin 0)
+  (scroll-conservatively 101)
+  (scroll-preserve-screen-position t)
+  (auto-window-vscroll nil)
+  (custom-file "~/.config/emacs-customize.el")
   :init
-  (setq minibuffer-prompt-properties
-	'(read-only t cursor-intangible t face minibuffer-prompt))
-  (setq completion-cycle-threshold 3)
-  (setq read-extended-command-predicate
-	#'command-completion-default-include-p)
-  (setq tab-always-indent 'complete)
+  (load custom-file)
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-  (setq enable-recursive-minibuffers t)
-  :config
-  (set-frame-font "Iosevka Custom 13" nil t)
-  (tab-bar-mode t)
-  (setq scroll-margin 0
-	scroll-conservatively 101
-	scroll-preserve-screen-position t
-	auto-window-vscroll nil)
+  (add-hook 'window-setup-hook 'toggle-frame-maximized t)
   (setq-default indent-tabs-mode nil
-		            tab-width ryl/indent-width))
-
-(use-package ligature
-  :config
-  ;; Enable the "www" ligature in every possible major mode
-  (ligature-set-ligatures 't '("www"))
-  ;; Enable traditional ligature support in eww-mode, if the
-  ;; `variable-pitch' face supports it
-  (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
-  (ligature-set-ligatures 'prog-mode '("-<<" "-<" "-<-" "<--" "<---" "<<-" "<-"
-                                       "->" "->>" "-->" "--->" "->-" ">-" ">>-"
-                                       "=<<" "=<" "=<=" "<==" "<===" "<<=" "<="
-                                       "=>" "=>>" "==>" "===>" "=>=" ">=" ">>="
-                                       "<->" "<-->" "<--->" "<---->" "<=>" "<==>"
-                                       "<===>" "<====>" "::" ":::" "__"
-                                       "<~~" "</" "</>" "/>" "~~>" "==" "!=" "<>"
-                                       "===" "!==" "!===" "<:" ":=" "*=" "*+"
-                                       "<*" "<*>" "*>" "<|" "<|>" "|>" "<." "<.>" ".>"
-                                       "+*" "=*" "=:" ":>" "(*" "*)" "/*" "*/"
-                                       "[|" "|]" "++" "+++" "\\/" "/\\" "|-" "-|"
-                                       "<!--" "<!---" "-*-"))
-  ;; Enables ligature checks globally in all buffers. You can also do it
-  ;; per mode with `ligature-mode'.
-  (global-ligature-mode t))
+                tab-width ryl/indent-width)
+  (set-fringe-mode 10))
 
 (use-package scroll-bar
-  :ensure nil
-  :config (scroll-bar-mode -1))
-
-(use-package cc-vars
-  :ensure nil
-  :config
-  (setq-default c-basic-offset ryl/indent-width)
-  (setq c-default-style '((awk-mode . "awk")
-			  (other . "k&r"))))
-
-(use-package lua-mode
-  :ensure nil
-  :config
-  (setq lua-indent-level 2))
-
-(use-package python
-  :ensure nil
-  :mode ("\\.py\\'" . python-mode)
-  :interpreter ("python" . python-mode)
-  :config (setq python-indent-offset ryl/indent-width))
-
-(use-package paren
-  :ensure nil
-  :init (setq show-paren-delay 0)
-  :config (show-paren-mode +1))
-
-(use-package elec-pair
-  :ensure nil
-  :hook (prog-mode . electric-pair-mode))
-
-(use-package all-the-icons
-  :if (display-graphic-p))
-
-(use-package dashboard
-  :config
-  (dashboard-setup-startup-hook)
-  :custom
-  (dashboard-startup-banner "~/.emacs.d/alanza.png")
-	(dashboard-banner-logo-title "You wake up early in the morning and you work all day; that's the only secret.")
-	(dashboard-center-content t)
-  (dashboard-items '((recents . 5)
-                     (bookmarks . 5)
-                     (agenda . 5)
-                     (registers . 5)))
-  (dashboard-icon-type 'all-the-icons)
-	(dashboard-set-heading-icons t)
-	(dashboard-set-file-icons t))
-
-(use-package highlight-escape-sequences
-  :hook (prog-mode . hes-mode))
+:ensure nil
+:config (scroll-bar-mode -1))
 
 (use-package evil
-  :diminish undo-tree-mode
   :init
   (setq evil-want-C-u-scroll t
-	evil-want-keybinding nil
-	evil-shift-width ryl/indent-width)
+        evil-want-keybinding nil
+        evil-shift-width ryl/indent-width)
   :hook (after-init . evil-mode)
   :preface
   (defun ryl/save-and-kill-this-buffer ()
@@ -155,66 +73,126 @@
   (evil-ex-define-cmd "wq" #'ryl/save-and-kill-this-buffer))
 
 (use-package evil-collection
-  :after evil
-  :config
-  (setq evil-collection-company-use-tng nil)
-  (evil-collection-init))
+  :after evil-mode
+  :custom evil-collection-company-use-tng nil
+  :config (evil-collection-init))
 
 (use-package evil-commentary
   :after evil
   :diminish
   :config (evil-commentary-mode +1))
 
-(use-package magit
-  :bind ("C-x g" . magit-status)
-  :config (add-hook 'with-editor-mode-hook #'evil-insert-state))
+(use-package diminish
+  :demand t)
 
-(use-package flycheck
-  :config (global-flycheck-mode +1))
+(use-package elcord
+  :demand t
+  :config (elcord-mode))
 
-(use-package eglot
-  :ensure nil)
+(defvar ryl/default-font-size 130)
+(set-face-attribute 'default nil :font "Iosevka Custom" :height ryl/default-font-size)
+(set-face-attribute 'fixed-pitch nil :font "Iosevka Custom" :height ryl/default-font-size)
+(set-face-attribute 'variable-pitch nil :font "Avenir Next" :height ryl/default-font-size)
+(set-frame-width nil 200)
+(set-frame-height nil 50)
 
-(use-package lsp-snippet-tempel
-  :straight (lsp-snippet-tempel :type git
-                                :host github
-                                :repo "svaante/lsp-snippet")
+(use-package ligature
   :config
-  (when (featurep 'eglot)
-    ;; Initialize lsp-snippet -> tempel in eglot
-    (lsp-snippet-tempel-eglot-init)))
+  (ligature-set-ligatures 'prog-mode
+                          '("<->" "<-->" "<--->" "<---->" "<----->"  "<=>" "<==>" "<===>"
+                            "<====>" "<=====>"  "<**>" "<***>" "<****>" "<*****>"
+                            "<!--" "<!---"  "<$" "<$>" "$>" "<." "<.>" ".>"
+                            "<*" "<*>" "*>" "<\\" "<\\>" "\\>" "</" "</>" "/>" "<\""
+                            "<\">" "\">" "<'" "<'>" "'>" "<^" "<^>" "^>" "<&" "<&>"
+                            "&>" "<%" "<%>" "%>" "<@" "<@>" "@>" "<#" "<#>" "#>"
+                            "<+" "<+>" "+>" "<-" "<->" "->" "<!" "<!>" "!>" "<?"
+                            "<?>" "?>" "<|" "<|>" "|>" "<:" "<:>" ":>"
+                            "::" ":::" "::::"  "->" "->-" "->--" "->>" "->>-"
+                            "->>--" "->>>" "->>>-" "->>>--" "-->" "-->-" "-->--" "-->>"
+                            "-->>-" "-->>--" "-->>>" "-->>>-" "-->>>--" ">-" ">--" ">>-"
+                            ">>--" ">>>-" ">>>--" "=>" "=>=" "=>==" "=>>" "=>>=" "=>>=="
+                            "=>>>" "=>>>=" "=>>>==" "==>" "==>=" "==>==" "==>>" "==>>="
+                            "==>>==" "==>>>" "==>>>=" "==>>>==" ">=" ">==" ">>=" ">>=="
+                            ">>>=" ">>>==" "<-" "-<-" "--<-" "<<-" "-<<-" "--<<-" "<<<-"
+                            "-<<<-" "--<<<-" "<--" "-<--" "--<--" "<<--" "-<<--" "--<<--"
+                            "<<<--" "-<<<--" "--<<<--" "-<" "--<" "-<<" "--<<" "-<<<"
+                            "--<<<" "<=" "=<=" "==<=" "<<=" "=<<=" "==<<=" "<<<=" "=<<<="
+                            "==<<<=" "<==" "=<==" "==<==" "<<==" "=<<==" "==<<==" "<<<=="
+                            "=<<<==" "==<<<==" "=<" "==<" "=<<" "==<<" "=<<<" "==<<<"
+                            ">=>" ">->" ">-->" ">==>" "<=<" "<-<" "<--<" "<==<"  ">>"
+                            ">>>" "<<" "<<<"  ":+" ":-" ":=" "+:" "-:" "=:" "=^"
+                            "=+" "=-" "=*" "=/" "=%" "^=" "+=" "-=" "*=" "/=" "%="
+                            "/\\" "--" "~~" "---" "~~~" "==" "==="
+                            "-----" "~~~~~" "-----------" "~~~~~~~~~~"
+                            "\\/"  "<>" "<+" "<+>" "+>"))
+  (global-ligature-mode t))
+
+(use-package cc-vars
+  :ensure nil
+  :custom
+  (c-default-style '((awk-mode . "awk")
+                     (other . "k&r")))
+  :config
+  (setq-default c-basic-offset ryl/indent-width))
+
+(use-package lua-mode
+  :ensure nil
+  :interpreter ("lua" . lua-mode)
+  :custom
+  (lua-indent-level 2))
+
+(use-package python
+  :ensure nil
+  :mode ("\\.py\\'" . python-mode)
+  :interpreter ("python" . python-mode)
+  :custom (python-indent-offset ryl/indent-width))
+
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(use-package dashboard
+  :config
+  (dashboard-setup-startup-hook)
+  :custom
+  (dashboard-startup-banner "~/.emacs.d/alanza.png")
+  (dashboard-banner-logo-title "You wake up early in the morning and you work all day; that's the only secret.")
+  (dashboard-center-content t)
+  (dashboard-items '((recents . 5)
+                     (bookmarks . 5)
+                     (agenda . 5)
+                     (registers . 5)))
+  (dashboard-icon-type 'all-the-icons)
+  (dashboard-set-file-icons t))
 
 (use-package org
   :mode ("\\.org\\'" . org-mode)
   :hook ((org-mode . visual-line-mode)
-	       (org-mode . org-indent-mode))
-  :config
-  (setq org-publish-project-alist
-      '(("ryleelyman.github.io" ;; my blog project (just a name)
-         ;; Path to org files.
-         :base-directory "~/Site/_org"
-         :base-extension "org"
-         ;; Path to Jekyll Posts
-         :publishing-directory "~/Site/_posts"
-         :recursive t
-         :publishing-function org-html-publish-to-html
-         :headline-levels 4
-         :html-extension "html"
-         :body-only t
-         ))))
+         (org-mode . org-indent-mode))
+  :custom
+  (org-publish-project-alist
+   '(("ryleelyman.github.io"
+      :base-directory "~/Site/_org"
+      :base-extension "org"
+      :publishing-directory "~/Site/_posts"
+      :recursive t
+      :publishing-function org-html-publish-to-html
+      :headline-levels 4
+      :html-extension "html"
+      :body-only t))))
+
+(with-eval-after-load 'org
+  (org-babel-do-load-languages
+      'org-babel-load-languages
+      '((emacs-lisp . t)
+        (python . t)
+        (lua . t)))
+
+  (push '("conf-unix" . conf-unix) org-src-lang-modes))
+
+(use-package conf-mode)
 
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode))
-
-(use-package diminish
-  :demand t)
-
-(use-package which-key
-  :diminish which-key-mode
-  :config
-  (which-key-mode +1)
-  (setq which-key-idle-delay 0.4
-	which-key-idle-secondary-delay 0.4))
 
 (use-package tex
   :ensure auctex
@@ -222,112 +200,52 @@
   :config
   (setq-default TeX-master "main")
   (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
-  (setq TeX-PDF-mode t
-        TeX-source-correlate-mode t
-        TeX-source-correlate-start-server t)
   (add-to-list 'TeX-expand-list
                '("%sn" (lambda () server-name)))
   (add-to-list 'TeX-view-program-list
-               '("Zathura"
+               '(("Zathura"
                  ("zathura "
-                  (mode-io-correlate " --synctex-forward %n:0:\"%b\" -x \"emacsclient --socket-name=%sn +%{line} %{input}\" ")
+                  (mode-io-correlate "--synctex-forward %n:0:\"%b\" -x \"emacsclient --socket-name=%sn +%{line} %{input}\" ")
                   "%o")
                  "zathura"))
-  (add-to-list 'TeX-view-program-list
-               '("Sioyek"
-                 ("sioyek"
-                 (mode-io-correlate " --inverse-search \"emacsclient --socket-name=%sn +%2 %1\" --forward-search-file \"%b\" --forward-search-line %n")
-                 " %o")
-                 "sioyek"))
+               ("Sioyek"
+                ("sioyek "
+                 (mode-io-correlate "--inverse-search \"emacsclient --socket-name=%sn +%2 %1\" --forward-search-file \"%b\" --forward-search-line %n ")
+                 "%o")
+                "sioyek"))
   (add-to-list 'TeX-view-program-selection
                '(output-pdf "Sioyek"))
-  (setq preview-image-type 'dvisvgm))
+  :custom
+  (TeX-PDF-mode t)
+  (TeX-source-correlate-mode t)
+  (TeX-source-correlate-start-server t)
+  (preview-image-type 'dvisvgm))
 
 (use-package auctex-latexmk
   :after tex
-  :config
-  (auctex-latexmk-setup))
-
-(use-package elcord
-  :demand t
-  :config
-  (elcord-mode))
-
-(use-package vertico
-  :init
-  (vertico-mode)
-  (setq vertico-resize t
-	vertico-cycle t))
-
-(use-package orderless
-  :init
-  (setq completion-styles '(orderless basic)
-	completion-category-defaults nil
-	completion-category-overrides '((file (styles partial-completion)))))
-
-(use-package corfu
-  :custom
-  (corfu-cycle t)
-  (corfu-auto t)
-  :config
-  (global-corfu-mode 1))
-
-(use-package tempel
-  :bind (("M-+" . tempel-complete) ;; Alternative tempel-expand
-         ("M-*" . tempel-insert))
-
-  :init
-
-  ;; Setup completion at point
-  (defun tempel-setup-capf ()
-    ;; Add the Tempel Capf to `completion-at-point-functions'.
-    ;; `tempel-expand' only triggers on exact matches. Alternatively use
-    ;; `tempel-complete' if you want to see all matches, but then you
-    ;; should also configure `tempel-trigger-prefix', such that Tempel
-    ;; does not trigger too often when you don't expect it. NOTE: We add
-    ;; `tempel-expand' *before* the main programming mode Capf, such
-    ;; that it will be tried first.
-    (setq-local completion-at-point-functions
-                (cons #'tempel-expand
-                      completion-at-point-functions)))
-
-  (add-hook 'prog-mode-hook 'tempel-setup-capf)
-  (add-hook 'text-mode-hook 'tempel-setup-capf)
-
-  ;; Optionally make the Tempel templates available to Abbrev,
-  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
-  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
-  ;; (global-tempel-abbrev-mode)
-)
-
-(use-package kind-icon
-  :ensure t
-  :after corfu
-  :custom
-  (kind-icon-default-face 'corfu-default)
-  :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
-(use-package dabbrev
-  :ensure nil
-  :bind (("M-/" . dabbrev-completion)
-	       ("C-M-/" . dabbrev-expand))
-  :custom
-  (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
+  :config (auctex-latexmk-setup))
 
 (use-package preview-dvisvgm
   :after tex)
 
-(use-package elcord
-  :demand t
-  :init (elcord-mode))
+(use-package paren
+  :ensure nil
+  :custom (show-paren-delay 0)
+  :config (show-paren-mode +1))
 
-(use-package moody
-  :config
-  (setq x-underline-at-descent-line t)
-  (moody-replace-mode-line-buffer-identification)
-  (moody-replace-vc-mode)
-  (moody-replace-eldoc-minibuffer-message-function))
+(use-package elec-pair
+  :ensure nil
+  :hook (prog-mode . electric-pair-mode))
+
+(use-package highlight-escape-sequences
+  :hook (prog-mode . hes-mode))
+
+(use-package flycheck
+  :config (global-flycheck-mode +1))
+
+(use-package magit
+  :bind ("C-x g" . magit-status)
+  :config (add-hook 'with-editor-mode-hook #'evil-insert-state))
 
 (use-package tramp
   :config
@@ -343,42 +261,57 @@
   (interactive)
   (magit-status "/yadm::"))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(LaTeX-font-list
-   '((1 "" "" "\\mathcal{" "}")
-     (2 "\\textbf{" "}" "\\mathbf{" "}")
-     (3 "\\textsc{" "}")
-     (5 "\\emph{" "}")
-     (6 "\\textsf{" "}" "\\mathsf{" "}")
-     (9 "\\textit{" "}" "\\mathit{" "}")
-     (12 "\\textulc{" "}")
-     (13 "\\textmd{" "}")
-     (14 "\\textnormal{" "}" "\\mathnormal{" "}")
-     (18 "\\textrm{" "}" "\\mathrm{" "}")
-     (19 "\\textsl{" "}" "\\mathbb{" "}")
-     (20 "\\texttt{" "}" "\\mathtt{" "}")
-     (21 "\\textup{" "}")
-     (23 "\\textsw{" "}")
-     (4 "" "" t)
-     (75 "" "" "\\mathfrak{" "}")))
- '(package-selected-packages
-   '(tempel-collection ligature lua-mode all-the-icons dap-C dap-mode lsp-treemacs lsp-ui exec-path-from-shell flycheck auctex-latexmk use-package corfu vertico lsp-mode elcord preview-dvisvgm preview-latex auctex evil))
- '(preview-LaTeX-command
-   '("%`xelatex --no-pdf \"\\nonstopmode\\nofiles\\PassOptionsToPackage{"
-     ("," . preview-required-option-list)
-     "}{preview}\\AtBeginDocument{\\ifx\\ifPreview\\undefined" preview-default-preamble "\\fi}\"%' \"\\detokenize{\" %(t-filename-only) \"}\""))
- '(preview-dvisvgm-pdf-command
-   "dvisvgm --libgs=/opt/homebrew/opt/ghostscript/lib/libgs.10.01.dylib --no-fonts _region_.xdv --page=- --output=\"%m/prev%%3p.svg\"")
- '(treesit-font-lock-level 4))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
 
-;;; init.el ends here
+(use-package corfu
+  :custom
+  (corfu-cycle t)
+  :config
+  (global-corfu-mode 1))
+
+(use-package kind-icon
+  :after corfu
+  :custom (kind-icon-default-face 'corfu-default)
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+(use-package vertico
+  :custom (vertico-resize t)
+  (vertico-cycle t)
+  :init (vertico-mode))
+
+(use-package tempel
+  :init
+  (defun tempel-setup-capf ()
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+  (add-hook 'prog-mode-hook 'tempel-setup-capf)
+  (add-hook 'text-mode-hook 'tempel-setup-capf))
+
+(use-package lsp-snippet-tempel
+  :straight (lsp-snippet-tempel :type git
+                                :host github
+                                :repo "svaante/lsp-snippet")
+  :config
+  (when (featurep 'eglot)
+    (lsp-snippet-tempel-eglot-init)))
+
+(use-package eglot
+  :ensure nil)
+
+(use-package which-key
+  :custom
+  (which-key-show-early-on-C-h t)
+  (which-key-idle-delay 2000)
+  (which-key-idle-secondary-delay 0.05)
+  :config (which-key-mode))
+
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-bubblegum t)
+  (kaolin-treemacs-theme))
